ba5d2a7544ff39e14c86b6a5fd8f61e7d86923e7
Ruvinda Dhambarage <ruvinda.dhambarage@nagarro.com>
2026-03-29 09:07:37 -0400

    Clean up the apt temp files more aggressively

--- src/amp/install.sh
+++ src/amp/install.sh
@@ -15,7 +15,7 @@ has_command curl || {
     apt-get update
     apt-get install -y --no-install-recommends curl ca-certificates
     apt-get clean
-    rm -rf /var/lib/apt/lists/*
+    rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 }
 
 # install Amp CLI
